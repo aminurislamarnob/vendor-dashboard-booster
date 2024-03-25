6efb7022e966db85a360b0a5b4a3531ff7e4d820
Md Aminur Islam <eng.aminurislam@gmail.com>
2024-03-26 00:04:30 +0600

    added vendor folder

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpenhancers/vendor-dashboard-booster',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '563f2d952c8876b46f1dd345896a0bb100b599fc',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wpenhancers/vendor-dashboard-booster' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '563f2d952c8876b46f1dd345896a0bb100b599fc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
